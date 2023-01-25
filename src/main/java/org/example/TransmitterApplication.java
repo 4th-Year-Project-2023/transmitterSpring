275d39344042608596ad99b3069d50644884f4c4
gregKraken <hvgreg01@gmail.com>
2023-01-25 22:08:23 +0530

    Feature : Added feature to close application after publishing
    Added feature to close application after publishing

--- src/main/java/org/example/TransmitterApplication.java
+++ src/main/java/org/example/TransmitterApplication.java
@@ -4,12 +4,14 @@ package org.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class TransmitterApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TransmitterApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(TransmitterApplication.class, args);
+        ctx.close();
     }
 
 
